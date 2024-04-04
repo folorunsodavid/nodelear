3872202f2df0a845da0ab708a1b3286b4f666b75
Folorunso David <dofoll@yahoo.com>
2024-04-04 12:34:27 +0100

    Cooling added

--- constr.js
+++ constr.js
@@ -1,13 +1,14 @@
 
-function Car(name, model, year, colour){
+function Car(name, model, year, colour, cooling){
     this.name = name
     this.model = model
     this.year = year
     this.colour = colour
+    this.cooling = cooling
     
 }
 
-const oko = new Car('Toyota','Echo', '2005', 'Black')
+const oko = new Car('Toyota','Echo', '2005', 'Black', 'Air Conditioner')
 
 // console.log(oko)
-console.log(oko.name, oko.model ,oko.year, oko.colour);
+console.log(oko.name, oko.model ,oko.year, oko.colour, oko.cooling);
