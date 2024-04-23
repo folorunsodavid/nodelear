d71b65d1dbfca9d1059f5d27bfc9ffd768b2f13c
Folorunso David <dofoll@yahoo.com>
2024-04-23 20:46:00 +0100

    new update

---
+++ constrClass.js
@@ -0,0 +1,15 @@
+class constrClass{
+    constructor (name){
+        this.name = name;
+        
+    }
+    
+    getName(){
+    return this.name;
+    }
+
+    setName(name){
+    this.name = name;
+    }
+}
+module.exports = constrClass
--- index.js
+++ index.js
@@ -7,14 +7,25 @@
 
 // MODULE EXPORTS
 // const add = require ('./add.js')
+const sis = require('../maths.js')
+// const addi = require('./maths.js')
 
 // console.log('Hello from index.js');
 // console.log('Charlie, e ye cool')
 
 // const sum = add(3,4)
 // console.log(sum)
-
+console.log(sis.big(3,4))
+console.log(sis.small(3,4))
 
 // MODULE SCOPE 
-require ('./batman')
-require ('./superman')
+// require ('./batman')
+// require ('./superman')
+
+// MODULE CHACHING
+// const learn = require ('./constrClass')
+
+// const kola = 'Vishwas'
+// learn.getName(kola)
+
+
