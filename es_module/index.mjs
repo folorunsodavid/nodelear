a24f2b577650c24772199ca2d1df3b54aa15640b
Folorunso David <dofoll@yahoo.com>
2024-04-24 01:58:11 +0100

    watch intialised

--- es_module/index.mjs
+++ es_module/index.mjs
@@ -2,5 +2,5 @@ import * as math from './math-esm.mjs';
 
 const {add, sub} = math;
 
-console.log(math.add(5,3));
-console.log(math.sub(5,3));
+console.log(math.add(5,4));
+console.log(math.sub(5,7));
